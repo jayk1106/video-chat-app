bb87cb5eef84577e1c7c67ef4c6c9a0d2fbb5a77
jay-me <kaneriyajay3@gmail.com>
2021-06-16 10:50:40 +0530

    video chat application deploy on heroku

---
+++ public/script.js
@@ -0,0 +1,239 @@
+const socket = io('/');
+
+const myPeer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: PORT,
+})
+
+
+myPeer.on('open', userId => {
+    socket.emit('join-room', ROOM_ID, userId , USER_NAME);
+})
+
+const bigPri = document.getElementById('bigPri');
+const smallPri = document.getElementById('smallPri');
+const guestVideo = document.createElement("video");
+
+
+
+
+
+// Invite Box
+// console.log(window.location.href);
+
+const roomurl = document.getElementById('roomurl');
+roomurl.innerHTML = `<span class='urlbox'>${window.location.href}</span>`;
+
+function openInviteBox(){
+    const inviteBox = document.getElementById('inviteBox');
+    inviteBox.style.display = 'flex';
+}
+function closeInviteBox(){
+    const inviteBox = document.getElementById('inviteBox');
+    inviteBox.style.display = 'none';
+}
+
+
+
+
+
+const allUsers = {};
+let localStream;
+
+// Create video element for user
+const myVideo = document.createElement('video');
+myVideo.setAttribute('id', 'small');
+myVideo.muted = true;
+
+var getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+//Access user's camara and microphone
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+}).then(myStream => {
+
+    localStream = myStream;
+    // To append stream to screen
+    myVideo.srcObject = myStream;
+    myVideo.addEventListener('loadedmetadata', () => {
+        myVideo.play();
+    })
+
+    smallPri.appendChild(myVideo);
+
+    // When guest connected
+    socket.on('guest-connected', (userId , userName) => {
+        guestConnectedMessage(userName);
+        socket.emit('recivedcall' , USER_NAME);
+        // Call guest when he connected
+        var call = myPeer.call(userId, myStream);
+        console.log("outgoing call", call);
+        // guestVideo.setAttribute('id', 'big');
+
+        call.on('stream', guestStream => {
+            // console.log("Outgoiing calls rosponse" ,guestStream);
+            guestVideo.srcObject = guestStream;
+            guestVideo.addEventListener('loadedmetadata', () => {
+                guestVideo.play();
+            })
+            const bigPri = document.getElementById('bigPri');
+            bigPri.appendChild(guestVideo);
+
+        })
+
+        call.on('close', () => {
+            guestVideo.remove();
+        })
+
+        allUsers[userId] = call;
+    })
+})
+
+
+socket.on('user-disconnect', (userId , name) => {
+    if (allUsers[userId]) allUsers[userId].close();
+    guestVideo.remove();
+    userLeaveMessage(name);
+})
+
+socket.on('guestJoindMessage' , name =>{
+    guestConnectedMessage(name);
+})
+
+let callStream;
+myPeer.on("call", function (call) {
+    console.log("incomming call outside" , call);
+    getUserMedia(
+      { video: true, audio: true },
+      function (myStream) {
+        callStream  = myStream;
+        call.answer(myStream); // Answer the call with an A/V stream.
+
+        call.on("stream", function (guestStream) {
+            guestVideo.srcObject = guestStream;
+            guestVideo.addEventListener('loadedmetadata' , () => {
+                guestVideo.play();
+            })
+            bigPri.appendChild(guestVideo);
+        });
+      },
+      function (err) {
+        console.log("Failed to get local stream", err);
+      }
+    );
+});
+
+
+
+
+socket.on('custom-err', msg => {
+    alert(msg);
+});
+
+function leaveMeetting() {
+    window.location.replace('/thankyou');
+}
+
+function playStopVideo() {
+    let enabled = localStream.getVideoTracks()[0].enabled;
+    const playstopvideo = document.getElementById('playstopvideo');
+    if (enabled) {
+        localStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo(playstopvideo);
+    } else {
+        setStopVideo(playstopvideo);
+        localStream.getVideoTracks()[0].enabled = true;
+    }
+    if(callStream){
+        let enabled2 = callStream.getVideoTracks()[0].enabled;
+        if (enabled2) {
+            callStream.getVideoTracks()[0].enabled = false;
+            setPlayVideo(playstopvideo);
+        } else {
+            setStopVideo(playstopvideo);
+            callStream.getVideoTracks()[0].enabled = true;
+        }
+    }
+
+}
+
+function muteUnmute() {
+    const enabled = localStream.getAudioTracks()[0].enabled;
+    const muteunmute = document.getElementById('muteunmute');
+    if (enabled) {
+        localStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton(muteunmute);
+    } else {
+        setMuteButton(muteunmute);
+        localStream.getAudioTracks()[0].enabled = true;
+    }
+    if(callStream){
+        let enabled2 = callStream.getAudioTracks()[0].enabled;
+        if (enabled2) {
+            callStream.getAudioTracks()[0].enabled = false;
+            setUnmuteButton(muteunmute);
+        } else {
+            setMuteButton(muteunmute);
+            callStream.getAudioTracks()[0].enabled = true;
+        }
+    }
+}
+
+function setUnmuteButton(div) {
+    div.innerHTML = '<i class="fas fa-microphone-slash"></i>';
+    div.style = "background-color:rgb(204, 34, 34);";
+}
+function setMuteButton(div) {
+    div.innerHTML = '<i class="fas fa-microphone"></i>';
+    div.style = "background-color:#0078d7;";
+}
+function setPlayVideo(video) {
+    video.innerHTML = '<i class="fas fa-video-slash" ></i>';
+    video.style = "background-color:rgb(204, 34, 34);";
+}
+function setStopVideo(video) {
+    video.innerHTML = '<i class="fas fa-video"></i>';
+    video.style = "background-color:#0078d7";
+}
+
+// Chat 
+const msgBox = document.getElementById('chatmessagebox');
+function sendMessage() {
+    const messageValue = document.getElementById('messageValue');
+    const message = messageValue.value;
+    if (message) {
+        appendMessage(message);
+        socket.emit('sendMessage', message , USER_NAME);
+        messageValue.value = "";
+    }
+}
+
+socket.on('reciveMessage', (msg , name) => {
+    const yourMsg = `<div class="guest__message">${name}: ${msg}</div>`;
+    msgBox.insertAdjacentHTML('beforeend', yourMsg);
+})
+
+
+function appendMessage(msg) {
+    const yourMsg = `<div class="your__message">You: ${msg}</div>`;
+    msgBox.insertAdjacentHTML('beforeend', yourMsg);
+}
+
+function guestConnectedMessage(name){
+    const yourMsg = `<div class="your__message">${name} Join</div>`;
+    msgBox.insertAdjacentHTML('beforeend', yourMsg);
+    const addNewUser = document.getElementById('addNewUser');
+    addNewUser.style.display = 'none';
+}
+
+function userLeaveMessage(name){
+    const yourMsg = `<div class="your__message">${name} Leaved</div>`;
+    msgBox.insertAdjacentHTML('beforeend', yourMsg);
+    const addNewUser = document.getElementById('addNewUser');
+    addNewUser.style.display = 'flex';
+}
---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+})
+
+const port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/peerjs', peerServer);
+
+app.get('/', (req, res, next) => {
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/thankyou' , (req, res, next) => {
+    res.render('thankyou');
+})
+
+app.get('/:roomId', (req, res, next) => {
+    res.render('index', { roomId: req.params.roomId , portNumber : port});
+})
+
+const allRoom = new Object();
+
+io.on('connection', socket => {
+
+    socket.on('join-room', (roomId, userId , userName) => {
+        
+        if(allRoom[roomId] == undefined) {
+            allRoom[roomId] = 0;
+        }    
+
+        if (allRoom[roomId] < 2) {
+            
+            socket.join(roomId);
+            allRoom[roomId]++;
+            
+
+            socket.broadcast.to(roomId).emit('guest-connected', userId , userName);
+            
+            socket.on('disconnect', () => {
+                socket.broadcast.to(roomId).emit('user-disconnect', userId , userName);
+                allRoom[roomId]--;
+                
+            })
+
+            socket.on('recivedcall' , otherUserName => {
+                socket.broadcast.to(roomId).emit('guestJoindMessage', otherUserName);
+            })
+            
+            socket.on('leaveMeetting', () => {
+                socket.broadcast.to(roomId).emit('user-disconnect', userId);
+                allRoom[roomId]--;
+            })
+            
+            socket.on('sendMessage' , (msg , name) => {
+                socket.broadcast.to(roomId).emit('reciveMessage' , msg , name);
+            })
+        }else{
+            socket.emit('custom-err' , 'You are not allowed to join this room');
+        }
+
+    })
+
+
+})
+
+server.listen(port, () => {
+    console.log("Server running on port "+port);
+})
